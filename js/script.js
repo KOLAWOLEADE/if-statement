e206527dd985d0a3ea1d5994872d7f9c309aa35a
Kolawole Adeyemo <kolawole_adeyemo@yahoo.com>
2022-05-28 16:47:44 +0100

    add if-statement

---
+++ js/script.js
@@ -0,0 +1,29 @@
+// if statement
+
+ let age = parseInt(prompt());
+ // from 0
+ if (age ===0){
+     alert("please enter a valid age")
+ }
+
+ // 1-17
+ else if (age <=17){
+     alert("you are still a teenager, enjoy while it last")
+ }
+
+//18-40
+ else if (age >= 18 && age <=40){
+    alert("life is too short");
+ }
+
+
+ // 40-60
+ else if (age > 40 && age <=60){
+      alert("remember a fool at 40 is a fool forever")
+  }
+
+
+  else{
+      alert("you should start preparing for the end !!! ")
+  }
+
